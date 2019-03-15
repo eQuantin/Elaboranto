0ca472a6b3c0c9fae09a188bf92c2173d62a15c8
Charline Fazentieux <chafazentieuxpro@gmail.com>
2019-03-15 14:59:12 +0100

    adding typography and dynamism on the js

--- gallery.js
+++ gallery.js
@@ -3,6 +3,7 @@ let modalBlock = document.querySelector('#modalImage');
 let span = document.querySelector('.close');
 let btnDown=document.querySelector('.fas');
 let galleryOne= document.querySelector('.gallery1');
+let downModule = document.querySelector('.downModule');
 
 modalBlock.onclick = _ => modal.style.display = 'flex', modal.style.alignItems = 'center', modal.style.flexDirection ='column';
 
@@ -11,7 +12,10 @@ span.onclick = _ => modal.style.display = 'none';
 const dropDown = _ => {
     if (galleryOne.style.display === 'grid'){
         galleryOne.style.display = 'none';
+        downModule.classList.add('rotate');
     } else {
-        galleryOne.style.display = 'grid' ;
+        galleryOne.style.display = 'grid';
+        downModule.classList.remove('rotate');
+
     }
 };
