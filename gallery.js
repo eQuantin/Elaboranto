10cb01ebe9d0eeaf4e523df2e9db45bc6dfd042e
Charline Fazentieux <chafazentieuxpro@gmail.com>
2019-03-15 10:47:00 +0100

    trying to apply to differents gallery some js

--- gallery.js
+++ gallery.js
@@ -1,17 +1,25 @@
-let modal = document.querySelector('#myModal');
-let modalBlock = document.querySelector('#modalImage');
+let modal = document.querySelector('.myModal');
+let modalBlock = document.querySelector('.modalImage');
 let span = document.querySelector('.close');
 let btnDown=document.querySelector('.fas');
 let galleryOne= document.querySelector('.gallery1');
 
-modalBlock.onclick = _ => modal.style.display = 'flex', modal.style.alignItems = 'center', modal.style.flexDirection ='column';
+
+// modalBlock.onclick = _ => modal.style.display = 'flex', modal.style.alignItems = 'center', modal.style.flexDirection ='column';
+
+let modalImage = _ => {
+    modal.style.display = 'flex'
+    modal.style.alignItems = 'center'
+    modal.style.flexDirection ='column';
+}
 
 span.onclick = _ => modal.style.display = 'none';
 
-const dropDown = _ => {
+let dropDown = _ => {
     if (galleryOne.style.display === 'grid'){
         galleryOne.style.display = 'none';
     } else {
         galleryOne.style.display = 'grid' ;
     }
 };
+
